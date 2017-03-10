0561739a1e933c09c310624cb0207e9b002f6730
Kin <winterheat@gmail.com>
2017-03-09 19:40:34 -0800

    move the initial spin velocity as a param for slot machine creation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 !(function() {
 
     $(function() {
-        new SlotMachine("#slot-machine button.spin", [
+        new SlotMachine("#slot-machine button.spin", 22, [
             { id: "#carousel1", numPanels: 9, images: ['images/coffee-maker.png', 'images/teapot.png', 'images/espresso-machine.png'] },
             { id: "#carousel2", numPanels: 9, images: ['images/coffee-filter.png', 'images/tea-strainer.png', 'images/espresso-tamper.png'] },
             { id: "#carousel3", numPanels: 9, images: ['images/coffee-grounds.png', 'images/tea-leaf.png', 'images/espresso-ground-beans.png'] }
--- js/slot-machine.js
+++ js/slot-machine.js
@@ -1,15 +1,19 @@
 
-function SlotMachine(selectorForSpin, arrSpinningWheelData) {
+function SlotMachine(selectorForSpin, initialSpinVelocity, arrSpinningWheelData) {
     var i, data, arrSpinningWheels = [];
 
     for (i = 0; i < arrSpinningWheelData.length; i++) {
         data = arrSpinningWheelData[i];
-        arrSpinningWheels.push(new SpinningWheel(data.id,
-                                                 data.numPanels,
-                                                 data.images,
-                                                 22,
-                                                 0.02 + 0.12 * (arrSpinningWheelData.length - i) + (Math.random() * 0.02 - 0.01) // plus or minus 0.01
-                                                )
+        arrSpinningWheels.push(
+                                new SpinningWheel(
+                                                   data.id,
+                                                   data.numPanels,
+                                                   data.images,
+                                                   initialSpinVelocity,
+                                                   // the following is deceleration. Note that
+                                                   // it is made so that initial spin wheel decelerate faster
+                                                   0.02 + 0.12 * (arrSpinningWheelData.length - i) + (Math.random() * 0.02 - 0.01) // plus or minus 0.01
+                                                 )
                               );
     }
 
